8d256ee7f225a37d61938cb611f0acc07134de83
Bulai Andrei-Benedict <bulaiandrei56@gmail.com>
2021-06-02 19:20:51 +0300

    lab4:ch4 stage1

---
+++ _4_exceptions_io/src/main/java/code/_4_student_effort/ch4/LCDMain.java
@@ -0,0 +1,63 @@
+package code._4_student_effort.ch4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LCDMain {
+    static String[] firstRow={
+            " _ ",  //0
+            "   ",  //1
+            " _ ",  //2
+            " _ ",  //3
+            "   ",  //4
+            " _ ",  //5
+            " _ ",  //6
+            " _ ",  //7
+            " _ ",  //8
+            " _ ",  //9
+    };
+    static String[] secondRow={
+            "| |",  //0
+            "  |",  //1
+            " _|",  //2
+            " _|",  //3
+            "|_|",  //4
+            "|_ ",  //5
+            "|_ ",  //6
+            "  |",  //7
+            "|_|",  //8
+            "|_|",  //9
+    };
+    static String[] lastRow={
+            "|_|",  //0
+            "  |",  //1
+            "|_ ",  //2
+            " _|",  //3
+            "  |",  //4
+            " _|",  //5
+            "|_|",  //6
+            "  |",  //7
+            "|_|",  //8
+            " _|",  //9
+    };
+    public static void main(String[] args) throws IOException {
+        Scanner scanner=new Scanner(new BufferedReader(new FileReader("./_test_files/in/input_number.txt")));
+        int n=scanner.nextInt();
+        String[] text={"","",""};
+        String strNum = "" + n;
+        int strLength = strNum.length(),digit;
+        for (int i = 0; i < strLength; ++i) {
+            digit = Integer.parseInt(String.valueOf(strNum.charAt(i)));
+            text[0]+=firstRow[digit];
+            text[1]+=secondRow[digit];
+            text[2]+=lastRow[digit];
+        }
+        scanner.close();
+        FileWriter fileWriter=new FileWriter("./_test_files/out/output_number.txt");
+        fileWriter.write(text[0]+'\n'+text[1]+'\n'+text[2]);
+        fileWriter.close();
+    }
+}
