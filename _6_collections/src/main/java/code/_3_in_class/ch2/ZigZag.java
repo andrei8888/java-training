c3eb6cf6fd316f0fae579bcceb21fe9d90e6827a
Bulai Andrei-Benedict <bulaiandrei56@gmail.com>
2021-06-28 23:08:20 +0300

    lab6:ch2

--- _6_collections/src/main/java/code/_3_in_class/ch2/ZigZag.java
+++ _6_collections/src/main/java/code/_3_in_class/ch2/ZigZag.java
@@ -1,2 +1,23 @@
-package code._3_in_class.ch2;public class ZigZag {
+package code._3_in_class.ch2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ZigZag {
+    public static void main(String[] args) {
+        List<Integer> elements= Arrays.asList(1,4,2,3,5,76,56,33,77,2);
+        Collections.sort(elements);
+        System.out.println(elements);
+
+        List newList=new ArrayList();
+        int n=elements.size();
+        for(int i=0;i<=n/2 && i<=n-i-1;i++){
+            newList.add(elements.get(i));
+            if(i!=n-i-1)
+                newList.add(elements.get(n-i-1));
+        }
+        System.out.println(newList);
+    }
 }
