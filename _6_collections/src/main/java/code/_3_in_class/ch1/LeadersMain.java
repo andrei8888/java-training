e92de015466b0c8ef72808b46d73f14df2165ca2
Bulai Andrei-Benedict <bulaiandrei56@gmail.com>
2021-06-28 23:07:33 +0300

    lab6:ch1

--- _6_collections/src/main/java/code/_3_in_class/ch1/LeadersMain.java
+++ _6_collections/src/main/java/code/_3_in_class/ch1/LeadersMain.java
@@ -1,2 +1,30 @@
-package code._3_in_class.ch1;public class LeadersMain {
+package code._3_in_class.ch1;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public class LeadersMain {
+    public static void displayLeaders(List<Integer> elements){
+        System.out.println(elements);
+        Collections.reverse(elements);
+        int max=elements.get(0);
+        for(int currentElement : elements)
+            if(currentElement>max){
+                System.out.print(currentElement + " ");
+                max=currentElement;
+            }
+    }
+
+    public static void main(String[] args) {
+        List elem=new ArrayList();
+        elem.add(7);
+        elem.add(5);
+        elem.add(3);
+        elem.add(2);
+        elem.add(1);
+        elem.add(1);
+        displayLeaders(elem);
+    }
 }
