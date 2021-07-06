eb2848867fbfdead6c174d07ab0c24345edcde5a
Bulai Andrei-Benedict <bulaiandrei56@gmail.com>
2021-07-06 11:46:36 +0300

    ch3 modified(alloc)

--- _6_collections/src/main/java/code/_4_student_effort/ch3/BellsTriangle.java
+++ _6_collections/src/main/java/code/_4_student_effort/ch3/BellsTriangle.java
@@ -12,13 +12,13 @@ public class BellsTriangle {
     }
 
     public static void main(String[] args) {
-        int[] array=new int[n];
-        int[] prev=new int[n-1];
+        int[] array=new int[1];
+        int[] prev;
         array[0]=1;
         display(array,1);
         for(int i=1;i<n;i++){
             prev=array;
-            array=prev.clone();
+            array=new int[i+1];
             array[0]=prev[i-1];
             for(int j=1;j<=i;j++)
                 array[j]=array[j-1]+prev[j-1];
