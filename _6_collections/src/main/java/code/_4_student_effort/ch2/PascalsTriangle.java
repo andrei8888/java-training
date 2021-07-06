59a91eff88c460315b7fddf6f53c774cc884a924
Bulai Andrei-Benedict <bulaiandrei56@gmail.com>
2021-07-06 11:45:42 +0300

    lab6:ch2

--- _6_collections/src/main/java/code/_4_student_effort/ch2/PascalsTriangle.java
+++ _6_collections/src/main/java/code/_4_student_effort/ch2/PascalsTriangle.java
@@ -1,2 +1,28 @@
-package code._4_student_effort.ch2;public class PascalsTriangle {
+package code._4_student_effort.ch2;
+
+public class PascalsTriangle {
+    static final int n=11;
+
+    private static void display(int[] arr,int line){
+        if(line%2==0 && n%2!=0 || line%2!=0 && n%2==0)
+            System.out.print(' ');
+        System.out.print("  ".repeat((n-line)/2));
+        for(int i=0;i<line;i++)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        int[] pascal=new int[1];
+        int[] prev;
+        for(int i=0;i<n;i++){
+            prev=pascal;
+            pascal=new int[i+1];
+            pascal[0]=1;
+            pascal[i]=1;
+            for(int j=1;j<i;j++)
+                pascal[j]=prev[j-1]+prev[j];
+            display(pascal,i+1);
+        }
+    }
 }
