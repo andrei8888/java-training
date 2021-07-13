d89807da2b057c896d003552cdee8c400bdbafce
Bulai Andrei-Benedict <bulaiandrei56@gmail.com>
2021-07-13 15:45:12 +0300

    first design pattern

--- PizzaBuilder/src/PizzaCreator.java
+++ PizzaBuilder/src/PizzaCreator.java
@@ -1,17 +1,21 @@
 public class PizzaCreator {
     public static void main(String[] args) {
-        PizzaMenuBuilder pizzasWithThickDough=new PizzaMenuBuilder(Dough.Thick);
-        pizzasWithThickDough.addTopping(Toppings.Mushrooms).addTopping(Toppings.Cheese).addTopping(Toppings.Ham);
-        pizzasWithThickDough.addDrink(Drink.Beer);
-        pizzasWithThickDough.addSauce(Sauce.Mayonnaise);
-        PizzaMenu pizza1=pizzasWithThickDough.create();
+        PizzaMenu pizza1=new PizzaMenuBuilder(Dough.Thick)
+                .addTopping(Toppings.Mushrooms)
+                .addTopping(Toppings.Cheese)
+                .addTopping(Toppings.Ham)
+                .addDrink(Drink.Beer)
+                .addSauce(Sauce.Mayonnaise)
+                .create();
         System.out.println(pizza1);
 
-        PizzaMenuBuilder pizzasWithThinDough=new PizzaMenuBuilder(Dough.Thin);
-        pizzasWithThinDough.addTopping(Toppings.Salami).addTopping(Toppings.Mozzarella).addTopping(Toppings.Olives);
-        pizzasWithThinDough.addDrink(Drink.Pepsi);
-        pizzasWithThinDough.addSauce(Sauce.Spicy_Ketchup);
-        PizzaMenu pizza2=pizzasWithThinDough.create();
+        PizzaMenu pizza2=new PizzaMenuBuilder(Dough.Thin)
+                .addTopping(Toppings.Salami)
+                .addTopping(Toppings.Mozzarella)
+                .addTopping(Toppings.Olives)
+                .addDrink(Drink.Pepsi)
+                .addSauce(Sauce.Spicy_Ketchup)
+                .create();
         System.out.println(pizza2);
     }
 }
